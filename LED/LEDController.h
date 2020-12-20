21727a0b8753079205b654ecbcee909691fbd36f
Henning Vehring <henning.vehring@gmail.com>
2020-12-20 21:54:12 +0100

    Add missing LED message structs

--- LED/LEDController.h
+++ LED/LEDController.h
@@ -43,6 +43,75 @@ public:
         float dim;
     };
 
+    struct ValueMessage
+    {
+        ValueMessage(const uint16_t &red, const uint16_t &green, const uint16_t &blue, const bool &raw = false) :
+            id(0x102)
+          , red(red)
+          , green(green)
+          , blue(blue)
+        {
+            this->raw = static_cast<bool>(raw & 0x01);
+        }
+
+        uint32_t id;
+        uint16_t red;
+        uint16_t green;
+        uint16_t blue;
+        uint8_t raw;
+    };
+
+    struct FilterMessage
+    {
+        FilterMessage(const bool &filter) :
+            id(0x103)
+        {
+            this->filter = filter ? 0x01 : 0x00;
+        }
+
+        uint32_t id;
+        uint8_t filter;
+    };
+
+    struct FilterValueMessage
+    {
+        FilterValueMessage(const float &capacitance, const float &impedance, const float &inductivity) :
+            id(0x104)
+          , capacitance(capacitance)
+          , impedance(impedance)
+          , inductivity(inductivity)
+        {}
+
+        uint32_t id;
+        float capacitance;
+        float impedance;
+        float inductivity;
+    };
+
+    struct FilterValueBufferMessage
+    {
+        FilterValueBufferMessage(const float &capacitance, const float &impedance, const float &inductivity,
+                                 const float &x1, const float &x2, const float &y1, const float &y2) :
+            id(0x105)
+          , capacitance(capacitance)
+          , impedance(impedance)
+          , inductivity(inductivity)
+          , x1(x1)
+          , x2(x2)
+          , y1(y1)
+          , y2(y2)
+        {}
+
+        uint32_t id;
+        float capacitance;
+        float impedance;
+        float inductivity;
+        float x1;
+        float x2;
+        float y1;
+        float y2;
+    };
+
 private:
     boost::asio::io_service service;
     boost::asio::ip::udp::socket socket;
