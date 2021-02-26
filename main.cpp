5bbcdbc6df658a05b21d368fbec220c9bb99466f
Henning Vehring <henning.vehring@gmail.com>
2021-02-26 20:10:02 +0100

    Go back to initial main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,27 @@
-#include <iostream>
+#include <boost/bind/bind.hpp>
 
-#include "LED/LEDController.h"
+#include "sensors/PCSensor.h"
+
+#include "userModes/ModeManager.h"
+
+void init(PCSensor* pc, ModeManager* manager)
+{
+    pc->onHandlerFunctions.push_back(boost::bind(&ModeManager::addMode, manager, UserMode::ePCMode));
+    pc->offHandlerFunctions.push_back(boost::bind(&ModeManager::removeMode, manager, UserMode::ePCMode));
+}
 
 int main (int, char**)
 {
-    // LEDController controller("192.168.0.116");
-    LEDController controller("192.168.0.115");
+    ModeManager manager;
+    manager.addMode(UserMode::eOffMode);
 
-    int red = 0x00;
-    int green = 0x00;
-    int blue = 0x00;
-    float dim = 1.0;
+    PCSensor pcSensor;
 
-    while(true)
+    init(&pcSensor, &manager);
+
+    while (true)
     {
-        std::cout << "Red (0-255)   ";
-        std::cin >> red;
-        std::cout << "Green (0-255) ";
-        std::cin >> green;
-        std::cout << "Blue (0-255)  ";
-        std::cin >> blue;
-        std::cout << "Dim (0.0-1.0) ";
-        std::cin >> dim;
-
-        controller.setColor(red, green, blue);
-        controller.setDim(dim);
+        usleep(600000000);
     }
 
     return 0;
