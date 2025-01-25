334b4ed478dac5b8d4f61b283e3f30e0a9328040
Henning Vehring <henning.vehring@gmail.com>
2025-01-25 21:31:35 +0100

    Replace PCSensor with MotionSensor

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@
 
 #include "hmi/HMI.h"
 
-void init(PCSensor* pc, MotionSensor* motion, ModeManager* manager)
+void init(MotionSensor* motion, ModeManager* manager)
 {
-    // Add PC handler
-    pc->onHandlerFunctions.push_back(boost::bind(&ModeManager::addMode, manager, UserMode::ePCMode));
-    pc->offHandlerFunctions.push_back(boost::bind(&ModeManager::removeMode, manager, UserMode::ePCMode));
+    // Add PC Handler
+    motion->addMotionHandler(2, boost::bind(&ModeManager::addMode, manager, UserMode::ePCMode));
+    motion->addMotionStopHandler(2, boost::bind(&ModeManager::removeMode, manager, UserMode::ePCMode), 11);
 
     // Add motion handler for bedroom
     motion->addMotionHandler(1, boost::bind(&ModeManager::addMode, manager, UserMode::eIlluminateBedroomMode));
@@ -25,10 +25,9 @@ int main (int, char**)
     ModeManager* manager = ModeManager::getInstance();
     manager->addMode(UserMode::eOffMode);
 
-    PCSensor pcSensor;
     MotionSensor motionSensor;
 
-    init(&pcSensor, &motionSensor, manager);
+    init(&motionSensor, manager);
 
     HMI::getInstance();
 
