a2a25858fda25f653e8402ce24421d1be7f7403e
Henning Vehring <henning.vehring@gmail.com>
2025-04-17 22:12:04 +0200

    Implement dimmer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <unistd.h>
 
 #include "LED/LEDManager.h"
+#include "LED/Dimmer.h"
 
 #include "sensors/PCSensor.h"
 #include "sensors/MotionSensor.h"
@@ -30,12 +31,42 @@ void switchHandler(bool on)
     }
 }
 
+void dimHandler(double brightness)
+{
+    ModeManager* manager = ModeManager::getInstance();
+    if (manager->getActiveMode(UserMode::eLEDDining) == UserMode::eIlluminateDiningMode)
+    {
+        LEDController* led = LEDManager::getInstance()->getUnit(LEDManager::eDiningRoom);
+        led->setFilter(true);
+        led->setDim(brightness);
+        led->setWhiteDim(brightness);
+    }
+    if (manager->getActiveMode(UserMode::eLEDLivingTV) == UserMode::eIlluminateLivingMode)
+    {
+        LEDController* led = LEDManager::getInstance()->getUnit(LEDManager::eLivingRoomTV);
+        led->setFilter(true);
+        led->setDim(brightness);
+        led->setWhiteDim(brightness);
+    }
+    if (manager->getActiveMode(UserMode::eLEDLiving) == UserMode::eIlluminateLivingMode)
+    {
+        LEDController* led = LEDManager::getInstance()->getUnit(LEDManager::eLivingRoom);
+        led->setFilter(true);
+        led->setDim(brightness);
+        led->setWhiteDim(brightness);
+    }
+}
+
 int main (int, char**)
 {
     ModeManager::getInstance()->addMode(UserMode::eOffMode);
 
+    Dimmer dimmer(0.45);
     Switch wohnzimmer("schalter_wohnzimmer");
-    wohnzimmer.addHandler(std::bind(switchHandler, std::placeholders::_1));
+    wohnzimmer.addHandler(std::bind(&Dimmer::switchChanged, &dimmer, std::placeholders::_1));
+
+    dimmer.setDimListener(std::bind(&dimHandler, std::placeholders::_1));
+    dimmer.setOnOffListener(std::bind(&switchHandler, std::placeholders::_1));
 
     CECControl::getInstance();
 
